3f44a3c2c4d1f756004b43ed5bc7b87ad12377b0
Halvro <haikalalfaro123@gmail.com>
2024-05-04 08:51:48 +0700

    Menambahkan API Mahasiswa

---
+++ modul10_1302220098/Controllers/MahasiswaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using modul10_1302220098;
+
+namespace modul10_1302220098.Controllers
+{
+    [Route("api / [controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> listMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa(" Haikal Alfaro","1302220098", new List<string> { "KPL", "PBO", "BD" },2022),
+            new Mahasiswa(" Naufal Ajhar","1302223132", new List<string> { "KPL", "PBO", "BD" }, 2022),
+            new Mahasiswa(" Muhammad Galang","1302224321", new List<string> { "KPL", "PBO", "BD" },2022),
+            new Mahasiswa(" Najwa Aulia","1302228765", new List<string> { "KPL", "PBO", "BD" }, 2022),
+        };
+        
+        
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return listMahasiswa;
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return listMahasiswa[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa mahasiswa)
+        {
+            listMahasiswa.Add(mahasiswa);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            listMahasiswa.RemoveAt(id);
+        }
+    }
+}
---
+++ modul10_1302220098/Mahasiswa.cs
@@ -0,0 +1,19 @@
+﻿namespace modul10_1302220098
+{
+    public class Mahasiswa
+    {
+        public string name { get; set; }
+        public string nim { get; set; }
+        public List<string> Course { get; set; }
+        public int year { get; set; }
+
+        public Mahasiswa(string name, string nim, List<string> Course, int year)
+        {
+            this.name = name;
+            this.nim = nim;
+            this.Course = Course;
+            this.year = year;
+
+        }
+    }
+}
